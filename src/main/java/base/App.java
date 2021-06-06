43e71399d294c3471f0c7aa48205ba590706d379
kiwibrie <brianne0128@gmail.com>
2021-06-05 22:20:52 -0500

    working base

--- src/main/java/base/App.java
+++ src/main/java/base/App.java
@@ -11,5 +11,42 @@ public class App {
 
     public static void main(String[] args) {
         App myApp = new App();
+
+        double subtotal = myApp.promptAmount();
+        String state = myApp.promptState();
+        double tax = 0;
+
+        if(state.equals("WI")){
+            tax = myApp.calculateTax(subtotal);
+        }
+
+        double total = myApp.calculateTotal(subtotal, tax);
+
+        myApp.printOutput(subtotal, tax, total);
+    }
+
+    public void printOutput(double subtotal, double tax, double total){
+        System.out.printf("The subtotal is $%.2f.\n" +
+                "The tax is $%.2f.\n" +
+                "The total is $%.2f.",
+                subtotal, tax, total);
+    }
+
+    public double calculateTax(double subtotal){
+        return subtotal * 0.055; //5.5%
+    }
+
+    public double calculateTotal(double subtotal, double tax){
+        return subtotal + tax;
+    }
+
+    public double promptAmount(){
+        System.out.print("What is the order amount? ");
+        return Double.parseDouble(in.nextLine());
+    }
+
+    public String promptState(){
+        System.out.print("What is the state? ");
+        return in.nextLine();
     }
 }
